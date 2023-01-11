0e12409566359ed09e8452da9e20cfa13a2519fd
unknown <goh.yenther@gmail.com>
2023-01-11 17:12:04 +0800

    update sayHello.js

--- sayHello.js
+++ sayHello.js
@@ -6,14 +6,6 @@ function execute() {
   console.log('Your first app is executed successfully');
 }
 
-// available memory
-const os = require('os');
-var totalMem = os.totalmem();
-var freeMem = os.freemem();
-console.log(`Total Memory: ${totalMem}`);
-console.log(`Used Memory: ${totalMem - freeMem}`);
-console.log(`Free Memory: ${freeMem}`);
-
 // exports
 module.exports.say = say;
 module.exports.execute = execute;
