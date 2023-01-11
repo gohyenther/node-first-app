a34b5b896acfcdc35aa90961837ea9eb9218ab37
unknown <goh.yenther@gmail.com>
2023-01-11 19:45:06 +0800

    finalise logger.js

--- logger.js
+++ logger.js
@@ -2,22 +2,34 @@
 const sayHelloVar = require('./sayHello');
 const http = require('http');
 
-
 // url
 const hostname = '127.0.0.1';
 const port = 3000;
 
-
 // set up server 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.write('Welcome to my first node app!\n')
-  res.end('In progress\n');
+  if (req.url === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.write('Welcome to my first node app!\n\n')
+    res.write('In progress\n');
+    res.end();
+  }
+
+  if (req.url === '/numbers') {
+    res.write(JSON.stringify([1, 2, 3]));
+    res.end();
+  }
 });
 
+// run server
 server.listen(port, hostname, () => {
   sayHelloVar.say('Yen Ther');
   sayHelloVar.execute();
   console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+// check for connection by new user
+server.on('connection', (socket) => {
+  console.log('New user connected..');
 });
